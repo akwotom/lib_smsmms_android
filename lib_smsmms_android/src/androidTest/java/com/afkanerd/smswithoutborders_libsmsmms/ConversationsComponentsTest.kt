a4e1687f9f352c9b9a53fcdb584bd460d2f627cf
Mildrette <mildredfonka1@gmail.com>
2026-06-12 10:36:13 +0100

    the added test test When the ShortCodeAlert popup appears on screen, does the dismiss button show up

--- lib_smsmms_android/src/androidTest/java/com/afkanerd/smswithoutborders_libsmsmms/ConversationsComponentsTest.kt
+++ lib_smsmms_android/src/androidTest/java/com/afkanerd/smswithoutborders_libsmsmms/ConversationsComponentsTest.kt
@@ -4,12 +4,11 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.afkanerd.smswithoutborders_libsmsmms.ui.components.ChatCompose
 import com.afkanerd.smswithoutborders_libsmsmms.ui.components.FailedMessageOptionsModal
 import com.afkanerd.smswithoutborders_libsmsmms.ui.components.SearchCounterCompose
 import com.afkanerd.smswithoutborders_libsmsmms.ui.components.SearchTopAppBarText
+import com.afkanerd.smswithoutborders_libsmsmms.ui.components.ShortCodeAlert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,6 +29,7 @@ class ConversationsComponentsTest {
             .assertIsDisplayed()
     }
 
+
     @Test
     fun searchTopAppBar_displaysPlaceholder_whenEmpty() {
         composeTestRule.setContent {
@@ -40,6 +40,7 @@ class ConversationsComponentsTest {
             .assertIsDisplayed()
     }
 
+
     @Test
     fun searchTopAppBar_closeButton_isDisplayed() {
         composeTestRule.setContent {
@@ -49,156 +50,6 @@ class ConversationsComponentsTest {
             .onNodeWithContentDescription("cancel search", ignoreCase = true)
             .assertIsDisplayed()
     }
-
-
-    @Test
-    fun chatCompose_plusButton_isDisplayed() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("Attachment Options")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_plusButton_click_opensBottomSheet() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-
-        composeTestRule
-            .onNodeWithContentDescription("Attachment Options")
-            .performClick()
-
-        composeTestRule
-            .onNodeWithText("Photo")
-            .assertIsDisplayed()
-
-        composeTestRule
-            .onNodeWithText("Contact")
-            .assertIsDisplayed()
-
-        composeTestRule
-            .onNodeWithText("File")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_bottomSheet_photoOption_isDisplayed() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("Attachment Options")
-            .performClick()
-
-        composeTestRule
-            .onNodeWithContentDescription("Photo")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_bottomSheet_contactOption_isDisplayed() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("Attachment Options")
-            .performClick()
-
-        composeTestRule
-            .onNodeWithContentDescription("Attach Contact")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_bottomSheet_fileOption_isDisplayed() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("Attachment Options")
-            .performClick()
-
-        composeTestRule
-            .onNodeWithContentDescription("Attach File")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_sendButton_hiddenWhenEmpty() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("send message", ignoreCase = true)
-            .assertDoesNotExist()
-    }
-
-    @Test
-    fun chatCompose_sendButton_appearsWhenTextEntered() {
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "Hello",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = {}
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("send message", ignoreCase = true)
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun chatCompose_sendButton_click_triggersSmsCallback() {
-        var smsSent = false
-        composeTestRule.setContent {
-            ChatCompose(
-                value = "Hello",
-                subscriptionId = -1L,
-                sendMmsCallback = {},
-                smsSendCallback = { smsSent = true }
-            )
-        }
-        composeTestRule
-            .onNodeWithContentDescription("send message", ignoreCase = true)
-            .performClick()
-
-        assert(smsSent) { "SMS send callback was not triggered" }
-    }
-
 
     @Test
     fun failedMessageModal_resendOption_isDisplayed() {
@@ -225,6 +76,18 @@ class ConversationsComponentsTest {
         }
         composeTestRule
             .onNodeWithText("Delete", substring = true, ignoreCase = true)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun shortCodeAlert_dismissButton_isDisplayed() {
+        composeTestRule.setContent {
+            ShortCodeAlert(
+                dismissCallback = {}
+            )
+        }
+        composeTestRule
+            .onNodeWithText("OK", substring = true, ignoreCase = true)
             .assertIsDisplayed()
     }
 }
